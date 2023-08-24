433310f346b75cf58ebaaab1f370c31b6dba14c8
Maksim <zatannal@student.21-school.ru>
2023-08-24 16:40:37 +0300

    feature/config-database/wip: added migrations

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zenorachi/dynamic-user-segmentation
+
+go 1.21
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"github.com/zenorachi/dynamic-user-segmentation/pkg/database/postgres"
+	"time"
+)
+
+type Config struct {
+	HTTP HTTPConfig
+	Auth AuthConfig
+	DB   postgres.DBConfig
+}
+
+type (
+	HTTPConfig struct {
+		Host         string
+		Port         string
+		ReadTimeout  time.Duration
+		WriteTimeout time.Duration
+	}
+
+	AuthConfig struct {
+		AccessTokenTTL  time.Duration
+		RefreshTokenTTL time.Duration
+		Salt            string
+		Secret          string
+	}
+)
