9733a251a4d1e320c09d839834bfca7b33e723a1
Maksim <zatannal@student.21-school.ru>
2023-08-24 18:05:00 +0300

    develop/wip: added server

---
+++ internal/server/server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"github.com/zenorachi/dynamic-user-segmentation/internal/config"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func New(cfg *config.Config, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:         fmt.Sprintf("%s:%s", cfg.HTTP.Host, cfg.HTTP.Port),
+			Handler:      handler,
+			ReadTimeout:  cfg.HTTP.ReadTimeout,
+			WriteTimeout: cfg.HTTP.WriteTimeout,
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
